8a1bb4e8bdb6b298e7bb9ad7cdc0b3454b846bc6
Kevin Colten <kevin@zollege.com>
2021-04-08 09:34:33 -0500

    Update searchable-select.js

--- resources/assets/js/searchable-select.js
+++ resources/assets/js/searchable-select.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   let url = window.location.pathname === '/autodialer/bulk' ? 'callerid' : 'autodialer/callerid'; 
 
   $('.verified_phone_numbers').select2({
+    minimumResultsForSearch: 20,
     placeholder: "Choose Verified Phone Number...",
     ajax: {
         url: url,
